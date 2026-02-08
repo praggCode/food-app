a9ea7ee47375d464ad34678a73f31dcd13ad6910
Progyan Sen <progyansen@gmail.com>
2026-02-09 01:54:02 +0530

    p2: starting frontend

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/user/register" element={<h1>User Register</h1>} />
+        <Route path="/user/login" element={<h1>User Login</h1>} />
+        <Route
+          path="/food-partner/register"
+          element={<h1>Foodpartner Login</h1>}
+        />
+        <Route
+          path="/food-partner/login"
+          element={<h1>Foodpartner Login</h1>}
+        />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
